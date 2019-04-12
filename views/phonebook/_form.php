724d5d8e8bddde1aeb70648ed2aab32b14216842
Vladimir <wrinfotel@mail.ru>
2019-04-12 12:33:50 +0300

    form

--- views/phonebook/_form.php
+++ views/phonebook/_form.php
@@ -17,17 +17,17 @@ use yii\widgets\Pjax;
 	<?php Pjax::begin(['id' => 'update_user']) ?>
     <?php $form = ActiveForm::begin(['options' => ['data-pjax' => true, 'class' => 'update-form'], 'action' => Url::to(['phonebook/update', 'id' => $model->id])]); ?>
 
-    <?= $form->field($model, 'name')->textInput(['maxlength' => true, 'required'=> true]) ?>
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'surname')->textInput(['maxlength' => true, 'required'=> true]) ?>
+    <?= $form->field($model, 'surname')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'patronymic')->textInput(['maxlength' => true, 'required'=> true]) ?>
+    <?= $form->field($model, 'patronymic')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'mobile')->textInput(['maxlength' => true, 'required'=> true]) ?>
+    <?= $form->field($model, 'mobile')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'home')->textInput(['maxlength' => true, 'required'=> true]) ?>
+    <?= $form->field($model, 'home')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'email')->textInput(['maxlength' => true, 'required'=> true]) ?>
+    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
 
     <div class="form-group">
         <?= Html::submitButton('Изменить', ['class' => 'btn btn-success', 'onclick' => 'saveChanges(); return false;']) ?>
